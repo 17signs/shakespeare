606b977a335a0e68f195e4984a45f5fe19a54550
17 signs <dev@17signs.com>
2013-03-21 17:56:16 +0100

    1st commit
    Initial commit to make system available

---
+++ spec/research_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+describe "Research user" do
+
+  it "should deliver result" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
